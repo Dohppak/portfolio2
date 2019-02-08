b6275448494a7b6fe30ea8f91a65cc97d0edd741
Dohppak <seung20764298@gmail.com>
2019-02-08 19:19:00 +0900

    delete Sidebar

--- src/templates/category-template.js
+++ src/templates/category-template.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { graphql } from 'gatsby';
 import Layout from '../components/Layout';
-import Sidebar from '../components/Sidebar';
+// import Sidebar from '../components/Sidebar';
 import Feed from '../components/Feed';
 import Page from '../components/Page';
 import Pagination from '../components/Pagination';
@@ -26,7 +26,7 @@ const CategoryTemplate = ({ data, pageContext }) => {
 
   return (
     <Layout title={pageTitle} description={siteSubtitle}>
-      <Sidebar />
+      {/* <Sidebar /> */}
       <Page title={category}>
         <Feed edges={edges} />
         <Pagination
